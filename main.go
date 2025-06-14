6a548620132a9382aaa35f8b6eece548ac265b1d
Jason Carter <jason.carter@mammothgrowth.com>
2025-06-14 14:04:06 -0400

    Add api validate_chirp

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"sync/atomic"
 )
@@ -22,6 +24,7 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", healthz)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.getMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.resetMetrics)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirp)
 
 	server := http.Server{
 		Addr:    ":8080",
@@ -36,6 +39,85 @@ func healthz(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func validateChirp(w http.ResponseWriter, req *http.Request) {
+
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	type errors struct {
+		Error string `json:"error"`
+	}
+
+	type returnVals struct {
+		Valid bool `json:"valid"`
+	}
+
+	// get json body into struct
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+
+	// handle error parsing parameters
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+
+		resBody := errors{
+			Error: "Something went wrong",
+		}
+
+		data, err := json.Marshal(resBody)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+		}
+
+		// write back response
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(data))
+		return
+	}
+
+	// handle chirpy - validate length and respond
+	chirpLength := len([]rune(params.Body))
+	// validate length of body - len() returns bytes not characters
+	if chirpLength <= 140 {
+		resBody := returnVals{Valid: true}
+
+		data, err := json.Marshal(resBody)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		// if data, err := json.Marshal(resBody); err == nil {
+		// 	w.Write([]byte(data))
+		// }
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(data))
+		return
+	}
+
+	if chirpLength > 140 {
+		resBody := errors{Error: "Chirp is too long"}
+
+		data, err := json.Marshal(resBody)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(data))
+		return
+	}
+
 }
 
 func (cfg *apiConfig) getMetrics(w http.ResponseWriter, req *http.Request) {
