87dff8f0da53899d95ed18ba45763a9c57efb2eb
Jason Carter <jason.carter@mammothgrowth.com>
2025-06-17 20:15:33 -0400

    Add some refactoring around response writer

--- main.go
+++ main.go
@@ -13,6 +13,12 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
+var bannedWords = map[string]bool{
+	"kerfuffle": true,
+	"sharbert":  true,
+	"fornax":    true,
+}
+
 func main() {
 
 	apiCfg := apiConfig{}
@@ -36,6 +42,45 @@ func main() {
 
 }
 
+func replaceProfanity(msg string, bannedWords map[string]bool) string {
+
+	words := strings.Fields(msg)
+	for _, word := range words {
+		// check if word in msg is a key in bannedWords and return value with is boolean
+		if bannedWords[strings.ToLower(word)] {
+			msg = strings.ReplaceAll(msg, word, "****")
+		}
+	}
+
+	return msg
+}
+
+func respondWithError(w http.ResponseWriter, httpStatusCode int, msg string) {
+
+	errorResponse := struct {
+		Error string `json:"error"`
+	}{Error: msg}
+	respondWithJSON(w, httpStatusCode, errorResponse)
+
+}
+
+func respondWithJSON(w http.ResponseWriter, httpStatusCode int, payload interface{}) {
+
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"error": "Something went wrong"}`))
+		return
+	}
+
+	w.WriteHeader(httpStatusCode)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(data))
+
+}
+
 func healthz(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
@@ -48,14 +93,6 @@ func validateChirp(w http.ResponseWriter, req *http.Request) {
 		Body string `json:"body"`
 	}
 
-	type errors struct {
-		Error string `json:"error"`
-	}
-
-	type returnVals struct {
-		CleanBody string `json:"cleaned_body"`
-	}
-
 	// get json body into struct
 	decoder := json.NewDecoder(req.Body)
 	params := parameters{}
@@ -64,86 +101,30 @@ func validateChirp(w http.ResponseWriter, req *http.Request) {
 	// handle error parsing parameters
 	if err != nil {
 		log.Printf("Error decoding parameters: %s", err)
-
-		resBody := errors{
-			Error: "Something went wrong",
-		}
-
-		data, err := json.Marshal(resBody)
-		if err != nil {
-			log.Printf("Error marshalling JSON: %s", err)
-		}
-
-		// write back response
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte(data))
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong")
 		return
 	}
 
 	// handle chirpy - validate length and respond
+	// len() returns bytes not characters
 	chirpLength := len([]rune(params.Body))
-	// validate length of body - len() returns bytes not characters
 	if chirpLength <= 140 {
 
-		chirp := params.Body
-		bannedWords := map[string]bool{
-			"kerfuffle": true,
-			"sharbert":  true,
-			"fornax":    true,
-		}
-		words := strings.Fields(chirp)
-		for _, word := range words {
-			if bannedWords[strings.ToLower(word)] {
-				chirp = strings.ReplaceAll(chirp, word, "****")
-			}
-		}
+		chirpCleaned := replaceProfanity(params.Body, bannedWords)
+		validResponse := struct {
+			CleanBody string `json:"cleaned_body"`
+		}{CleanBody: chirpCleaned}
 
-		/*
-			cleanChirp := make([]string, len(chirp))
-			badWords := []string{"kerfuffle", "sharbert", "fornax"}
-			for _, word := range badWords {
-				if strings.Contains(strings.ToLower(chirp), word) {
-					chirpWords := strings.Split(chirp, " ")
-					for i, w := range chirpWords {
-						if word == strings.ToLower(w) {
-							cleanChirp[i] = "****"
-						} else {
-							cleanChirp[i] = w
-						}
-					}
-					chirp = strings.Join(cleanChirp, " ")
-				}
-			}
-		*/
-
-		resBody := returnVals{CleanBody: strings.Trim(chirp, "")}
-
-		data, err := json.Marshal(resBody)
-		if err != nil {
-			log.Printf("Error marshalling JSON: %s", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		w.WriteHeader(http.StatusOK)
-		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte(data))
+		respondWithJSON(w, http.StatusOK, validResponse)
 		return
 	}
 
 	if chirpLength > 140 {
-		resBody := errors{Error: "Chirp is too long"}
+		errorResponse := struct {
+			Error string `json:"error"`
+		}{Error: "Chirp is too long"}
 
-		data, err := json.Marshal(resBody)
-		if err != nil {
-			log.Printf("Error marshalling JSON: %s", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusBadRequest)
-		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte(data))
+		respondWithJSON(w, http.StatusOK, errorResponse)
 		return
 	}
 
